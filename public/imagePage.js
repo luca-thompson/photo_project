a4a2789a3a27725232a34219c71fb448641b80c6
luca-thompson <lucathompson006@gmail.com>
2026-04-04 22:26:50 +1100

    absolute url + CORS headers

--- public/imagePage.js
+++ public/imagePage.js
@@ -1,5 +1,5 @@
 function setPage(){
     const fname = location.href.split('=')[1]
-    document.getElementById("main").src=("media.abcphotos.xyz/" + fname[0] + '/' + fname + ".jpg");
+    document.getElementById("main").src=("https://media.abcphotos.xyz/" + fname[0] + '/' + fname + ".jpg");
     document.getElementById("imgMark").textContent=fname;
 }
